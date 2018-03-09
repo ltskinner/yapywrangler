6b0ac0185346fb00e15835874afb7c534467ff46
ltskinner <ltskinner@crimson.ua.edu>
2018-03-09 00:48:36 -0600

    updated lots, still testing

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # yapywrangler
 Stock market historical data collection and management system, pulling data from the updated Yahoo Finance backend, 2018
+
+
+Usage:
+
+readExisting(symbols, end=None)
+    Attempts to pull data from the /YPData/ folder.
+    If a specified symbol does not have any data, creates a new get request and saves the data
+    If data for specifed symbol does exist, makes sure it is the most up to dat day, and if not creates a new get request to update existing
+    
+    Returns dict
+
+securityData(symbols, end, save, epoch)
+    A direct get request with optional parameters to save
+
+    Returns dict
+
+
+
+data = readExisting(['MSFT'], end='2007-01-01')
+
+data2 = collectData(symbols=['ADBE', 'AMD'], end='2010-01-01', save=True, epoch=False)
--- yapywrangler.py
+++ yapywrangler.py
@@ -11,10 +11,16 @@ import datetime
 NEED TO THINK ABOUT TAKING DIRECTORY ARGUMENTS
 people cloning in wont want to dev in the clone, taking external directory arg would be good
 """
+"""
+Will want to make this a class so I can make sessin with servers as self.session = resp.sess()
+this will cut down ssl sync times
+"""
+
+
 
 
 def dataOrganizer(raw):
-    try:
+    try: 
         data = {
             'date': [],
             'open': [],
@@ -26,10 +32,9 @@ def dataOrganizer(raw):
 
         ct = 0
         for j in raw:
-            #print(j)
             if "amount" not in j:
                 each = j.split(',')
-                if ct ==0:
+                if ct == 0:
                     each[0] = each[0].strip("[{")
                 buffer = [  each[0].strip('"date":'), 
                             each[1].strip('"open":'), 
@@ -37,26 +42,13 @@ def dataOrganizer(raw):
                             each[3].strip('"low":'), 
                             each[4].strip('"close":'), 
                             each[5].strip('"volume":').strip('}]')]
-
                 #print(buffer)
 
-                run = True
-                for x in buffer:
-                    try:
-                        t = float(x)
-                    except:
-                        try:
-                            x.is_int()
-                        except:
-                            run = False
+                #sequence to catch null, nu, na, nan values
+                run = nullCatch(buffer)
 
                 if run:
-                    data['date'].append(buffer[0])
-                    data['open'].append(buffer[1])
-                    data['high'].append(buffer[2])
-                    data['low'].append(buffer[3])
-                    data['close'].append(buffer[4])
-                    data['volume'].append(buffer[5])
+                    finalForm(data, [buffer], epoch=True)
                     
             ct+=1
 
@@ -65,11 +57,12 @@ def dataOrganizer(raw):
     except Exception as e:
         print(e)
     
+# OK
 def getReq(sym, end):
     try:
         r = requests.get("https://finance.yahoo.com/quote/" + sym + "/history?period1=" + end + "&period2=" + str(int(time.time())) + "&interval=1d&filter=history&frequency=1d")
-        shart = (str(r.content).split("HistoricalPriceStore")[1]).split(',"isPending"')[0]
-        return shart.split('{"prices":')[1].split('},{')
+        lvl1 = (str(r.content).split("HistoricalPriceStore")[1]).split(',"isPending"')[0]
+        return lvl1.split('{"prices":')[1].split('},{')
     except:
         #if there is another year to bump up, it runs again, else returns empty list
         if ((int(end) + 31536000) < int(time.time())):
@@ -79,10 +72,9 @@ def getReq(sym, end):
             print("[-] NO DATA AVAILABLE [-]")
             return []
 
-
+# OK
 def writeCsv(sym, data):
     directory = os.getcwd() + '/YPData/'
-    #print(directory)
     try:
         os.stat(directory)
     except:
@@ -96,9 +88,8 @@ def writeCsv(sym, data):
         writer = csv.writer(f, delimiter=",")
         for line in writelist:
             writer.writerow(line)
-            #writer.write('\n')
     
-    print('[+]', sym.upper(), 'Saved. [+]')
+    print('[+]', sym.upper(), 'saved [+]')
 
 def readExisting(symbols, end=None):
     #reads from path and tries to open csv
@@ -110,11 +101,18 @@ def readExisting(symbols, end=None):
         dt = datetime.datetime.strptime(end, '%Y-%m-%d')
         end = str(int(time.mktime(dt.timetuple())))
         for sym in symbols:
-            data[sym] = {}
+            data[sym] = {
+                'date': [],
+                'open': [],
+                'high': [],
+                'low': [],
+                'close': [],
+                'volume': []
+            }
             buffer = []
             backboard = False
             try:
-                directory = '/YPData/' + sym.upper() + '_historical.csv'
+                directory = os.getcwd() + '/YPData/' + sym.upper() + '_historical.csv'
                 with open(directory, 'r') as f:
                     infile = csv.reader(f)
                     for line in infile:
@@ -122,91 +120,88 @@ def readExisting(symbols, end=None):
                             buffer.append(line)
                         else:
                             backboard = True
-            except:
+                        
+
+
+            except Exception as e:
+                #print(e)
                 backboard == False
             
             if backboard == False:
-                getdata = collectData([sym], save=True)
+                getdata = securityData([sym], save=True, epoch=False)
                 data[sym] = getdata[sym]
-                print('called collectData')
 
             else:
                 for i in buffer[0]:
                     data[sym][i] = []
-                
-                del buffer[0]
+                del buffer[0] #removes headers
 
-                for i in buffer:
-                    data[sym]['date'].append(time.strftime('%Y-%m-%d', time.localtime(int(i[0]))))
-                    data[sym]['open'].append(i[1])
-                    data[sym]['high'].append(i[2])
-                    data[sym]['low'].append(i[3])
-                    data[sym]['close'].append(i[4])
-                    data[sym]['volume'].append(i[5])
+
+                #if most recent day is more than a day old
+                if (int(time.time()) - int(buffer[0][0])) > 86400:
+                    getdata = securityData([sym], save=True, epoch=False)
+                    data[sym] = getdata[sym]
+                else:
+                    finalForm(data[sym], buffer)
 
     else:
         for sym in symbols:
-            data[sym] = {}
+            data[sym] = {
+                'date': [],
+                'open': [],
+                'high': [],
+                'low': [],
+                'close': [],
+                'volume': []
+            }
             buffer = []
-            exists = True
             try: 
-                directory = '/YPData/' + sym.upper() + '_historical.csv'
+                directory = os.getcwd() + '/YPData/' + sym.upper() + '_historical.csv'
                 with open(directory, 'r') as f:
                     infile = csv.reader(f)
                     for line in infile:
                         buffer.append(line)
             
-
                 for i in buffer[0]:
                     data[sym][i] = []
-                
                 del buffer[0]
+                
+                
+                #if most recent day is more than a day old
+                if (int(time.time()) - int(buffer[0][0])) > 86400:
+                    getdata = securityData([sym], save=True, epoch=False)
+                    data[sym] = getdata[sym]
+                
+                else:
+                    finalForm(data[sym], buffer)
 
-                for i in buffer:
-                    data[sym]['date'].append(time.strftime('%Y-%m-%d', time.localtime(int(i[0]))))
-                    data[sym]['open'].append(i[1])
-                    data[sym]['high'].append(i[2])
-                    data[sym]['low'].append(i[3])
-                    data[sym]['close'].append(i[4])
-                    data[sym]['volume'].append(i[5])
-
-            except:
-                getdata = collectData([sym], save=True)
-                data[sym] = getdata[sym]
-                print('called collectData')
-                time.sleep(10)
+            except Exception as e:
+                #print(e)
+                getdata = securityData([sym], save=True, epoch=False)
+                daa[sym] = getdata[sym]
             
 
 
     return data
 
-
-
-def collectData(symbols, end=None, save=False, epoch=True):
-    #unless specified, farthest back date is Jan 1 2008
+def securityData(symbols, end=None, save=False, epoch=True):
+    #unless specified, farthest back date is Jan 1 2000
     # converts user input to epoch time
     if end != None:
         dt = datetime.datetime.strptime(end, '%Y-%m-%d')
         end = str(int(time.mktime(dt.timetuple())))
     else:
-        end="1199145600"
+        end="946684800"
 
     #try to read in existing
     # takes the first day and if today is more than one days length ahead of it, continue in CollectData
     # else, reads in the existing file 
 
-    #need to make this dynamic, may want to incorporate collectData(daysback) type deal then figure out that date
-    #                    will also need to make some buffers and maintain local list of live symbols to reduce symbols?
-
-    #make http handler with urllib here, pass to yahoo req, make single session so we arent reconnection ssl each time.
-    #have a feeling this is the mian limiter
-    #spoof user agent for maybe server priority?
-    #see about turning this into a direct json 
     data = {}
     for sym in symbols:
         parsed = getReq(sym, end)
-        #divvied = parsed #.split('},{')
         data[sym] = dataOrganizer(parsed)
+
 
     if save:
         for sym in symbols:
@@ -220,33 +215,41 @@ def collectData(symbols, end=None, save=False, epoch=True):
 
     return data
 
-    """
-    need to update these to write line by line i reckon
-    df = pd.DataFrame(data) 
-    csv_name = sym + "_historical.csv"
-    col_order = ['date', 'open', 'high', 'low', 'close', 'volume']
-    df[col_order].to_csv('/home/lee/Projects/Finance/Data/Historical/' + csv_name,index=False)
-    #print "[+++] " + csv_name + " written [+++]"
-    """
+def nullCatch(buffer):
+    for x in buffer:
+        try:
+            t = float(x)
+        except:
+            try:
+                x.is_int()
+            except:
+                return False
+    return True
+
+def finalForm(data, buffer, epoch=False):
+    for i in buffer:
+        if epoch:
+            data['date'].append(int(i[0]))
+        else:
+            data['date'].append(time.strftime('%Y-%m-%d', time.localtime(int(i[0]))))
+
+        data['open'].append(i[1])
+        data['high'].append(i[2])
+        data['low'].append(i[3])
+        data['close'].append(i[4])
+        data['volume'].append(i[5])
+
+    return data 
 
 
-"""
-data = readExisting(['MSFT'], end='2007-01-01')
-print(data)
-"""
-"""
-start = datetime.datetime.now()
-data = collectData(symbols=['ADBE'])
-print(datetime.datetime.now() - start)
-for i in data:
-    print(i)
-"""
-
-
+data = readExisting(['MSFT', 'AMD', 'GGB'], end='2007-01-01')
+print(data['GGB']['date'][4])
+print(data['AMD']['date'][4])
+print(data['MSFT']['date'][4])
 """
 start2 = datetime.datetime.now()
 #                                    'Jul 9, 2009'
-data2 = collectData(symbols=['ADBE', 'AMD'], end='2010-01-01', save=True, epoch=False)
+data2 = securityData(symbols=['ADBE', 'AMD'], end='2010-01-01', save=True, epoch=False)
 print(datetime.datetime.now() - start2)
 
 for i in data2:
@@ -254,5 +257,5 @@ for i in data2:
 print(data2)
 #print('no end arg', len(data['ADBE']['open']))
 print('w/ end arg', len(data2['ADBE']['open']))
-"""
 
+"""
