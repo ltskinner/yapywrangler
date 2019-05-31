6c75cddeeb4df0e4b9a9ddc11c41be5b91a424bc
unknown <ltskinner@crimson.ua.edu>
2019-05-31 12:46:15 -0500

    connected readme to setup

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 from setuptools import setup
 
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(name='yapywrangler',
       version='1.1',
       description='Tool for reqesting stock data from the Yahoo Finance API',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       url='https://github.com/ltskinner/yapywrangler',
       author='ltskinner',
       license='MIT',
