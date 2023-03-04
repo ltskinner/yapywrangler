f1cf8ff15a0021b571c9fc4b0b68b3f93e894e61
ltskinner <ltskinner@crimson.ua.edu>
2023-03-04 11:04:45 -0600

    not sure what the changes are

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='yapywrangler',
-      version='1.1',
+      version='1.2',
       description='Tool for reqesting stock data from the Yahoo Finance API',
       long_description=long_description,
       long_description_content_type='text/markdown',
